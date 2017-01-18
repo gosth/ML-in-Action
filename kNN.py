916086d80b02ff7e391cb73b6200f2d1511e1813
gosth <danxuedexing@qq.com>
2017-01-19 00:15:44 +0800

    测试算法：验证分类器

--- kNN.py
+++ kNN.py
@@ -12,8 +12,9 @@ def classify0(inX, dataSet, labels, k):
 	for i in range(k):
 		voteIlabel = labels[sortedDistIndicies[i]]
 		classCount[voteIlabel] = classCount.get(voteIlabel,0)+1
-	sortedClassCount =sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+	sortedClassCount = (sorted(classCount.items(),key=operator.itemgetter(1)                            ,reverse=True))
 	return sortedClassCount[0][0]
+
 def createDataSet():
 	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
 	labels = ['A','A','B','B']
@@ -43,3 +44,18 @@ def autoNorm(dataSet):
     normDataSet = dataSet - tile(minVals, (m,1))
     normDataSet = normDataSet / tile(ranges, (m,1))
     return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = (classify0(normMat[i,:],normMat[numTestVecs:m,:],
+                            datingLabels[numTestVecs:m],3))
+        print ("the classifier came back with: %d, the real answer is: %d"
+               % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount / float(numTestVecs))
