5a76b4620163ee7c1bf47281d3531bc462fb4701
gosth <danxuedexing@qq.com>
2017-03-10 00:16:31 +0800

    handwritingClassTest函数未调通

--- kNN.py
+++ kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import operator
+from os import listdir
 
 def classify0(inX, dataSet, labels, k):
 	dataSetSize =dataSet.shape[0]
@@ -79,3 +80,32 @@ def img2vector(filename):
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('traningDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('testDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUndertest, trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is: %d"\
+               % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))    
